b570c2826e11ba491ecaf1a550324953d88c045a
unknown <dane.hoffman90@gmail.com>
2020-05-08 11:58:10 -0600

    initial commit

---
+++ FizzBuzzProj/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for (double count = 0; count < 101; count++) 
+            {
+                Console.WriteLine(count);
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
